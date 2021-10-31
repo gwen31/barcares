8d8463b6a30785c8198dfc669f402c2196a30736
gwen31 <merlegwenaelle026@gmail.com>
2021-10-31 22:56:59 +0100

    add photo + modif header

--- src/components/Telechargement.jsx
+++ src/components/Telechargement.jsx
@@ -7,16 +7,16 @@ const Telechargement = () => {
             <h2>Téléchargement</h2>
             <p>Vous pouvez téléchargé le bail de location et l'état des lieux ici</p>
             <div className={css(styles.subcontainer)}>
-            <div className={css(styles.telechargment)}>
-                <h3>Bail de location</h3>  
-                    <button className={css(styles.button)}><a className={css(styles.lien)} href="/media/Modele_Contrat_Location_Saisonniere-1.doc" download="contrat de location"> Contrat de location format Word </a></button>
-                     <button className={css(styles.button)}><a className={css(styles.lien)} href="/media/Modele_Contrat_Locationbail-saisonnier.pdf" download="contrat de location"> Contrat de location format pdf</a></button>     
-            </div>
-            <div className={css(styles.telechargment)}>
-                <h3>Etat des lieux</h3>
-                    <button className={css(styles.button)}><a className={css(styles.lien)} href="/media/Etat-des-lieux-T2-T3-simplifie.pdf" download="Etat des lieux T2 T3">Etat des lieux T2 T3 simplifié</a></button>
-                    <button className={css(styles.button)}><a className={css(styles.lien)} href="/media/Etat_des_lieux_annexe_mobilier.pdf" download="Etat des lieux annexes mobilier">Etat des lieux annexe mobilier</a></button>
-            </div>
+                <div className={css(styles.telechargment)}>
+                    <h3>Bail de location</h3>  
+                        <div><a className={css(styles.lien)} href="/media/Modele_Contrat_Location_Saisonniere-1.doc" download="contrat de location"> Contrat de location <img src="/assets/logos/docx_mac-256_32_35919.png" height="60px" width="60px" alt="logo format docx" /></a></div>
+                        <div><a className={css(styles.lien)} href="/media/Modele_Contrat_Locationbail-saisonnier.pdf" download="contrat de location"> Contrat de location <img src="/assets/logos/file_type_pdf_icon_130274.png" height="90px" width="90px" alt="logo format pdf" /></a></div>     
+                </div>
+                <div className={css(styles.telechargment)}>
+                    <h3>Etat des lieux</h3>
+                        <div><a className={css(styles.lien)} href="/media/Etat-des-lieux-T2-T3-simplifie.pdf" download="Etat des lieux T2 T3">Etat des lieux T2 T3 simplifié <img src="/assets/logos/file_type_pdf_icon_130274.png" height="90px" width="90px" alt="logo format pdf" /></a></div>
+                        <div><a className={css(styles.lien)} href="/media/Etat_des_lieux_annexe_mobilier.pdf" download="Etat des lieux annexes mobilier">Etat des lieux annexe mobilier <img src="/assets/logos/file_type_pdf_icon_130274.png" height="90px" width="90px" alt="logo format pdf" /></a></div>
+                </div>
             </div>
         </div>
        
@@ -55,8 +55,7 @@ const styles = StyleSheet.create({
         width: '60%',
         margin: 'auto',
         marginTop: '60px',
-        border: '1px solid black',
-        boxShadow: '8px 8px 10px 0 rgba(0,0,0,0.5)',
+       
         },
         "@media (max-width: 768px)":{
             width: '100%',
@@ -65,54 +64,27 @@ const styles = StyleSheet.create({
             flexWrap: 'wrap',
             margin: 'auto',
             marginTop: '60px',
-            border: '1px solid black',
-            boxShadow: '8px 8px 10px 0 rgba(0,0,0,0.5)'
         },
     },
     telechargment:{
         "@media (min-width: 600px)":{
-        width: '100%',
+        width: '70%',
         margin: 'auto',
         marginTop: '20px',
         padding: '30px',
+        border: '1px solid black',
+        boxShadow: '8px 8px 10px 0 rgba(0,0,0,0.5)',
         },
         "@media (max-width: 768px)":{
         width: '100%',
         margin: 'auto',
         marginTop: '20px',
         padding: '30px',
+        border: '1px solid black',
+        boxShadow: '8px 8px 10px 0 rgba(0,0,0,0.5)',
         },
     }, 
-    button:{
-        "@media (min-width: 600px)":{
-            display: 'flex',
-            flexDirection:'column',
-            alignItems: 'center',
-            margin: 'auto',
-            marginTop: '40px',
-            width: '80%',
-            fontSize: '20px',
-            padding: '10px',
-            backgroundColor: '#adb3d6',
-            borderRadius: '8px 8px 8px 8px',
-            ':hover':{
-            opacity: 0.5
-            },
-    },
-        "@media (max-width: 600px)":{
-            fontSize: '18px',
-            display: 'flex',
-            flexDirection:'column',
-            alignItems: 'center',
-            margin: 'auto',
-            marginTop: '40px',
-            width: '100%',
-            fontSize: '20px',
-            padding: '10px',
-            backgroundColor: '#adb3d6',
-            borderRadius: '8px 8px 8px 8px',
-            },
-    },
+   
     lien:{
         textDecoration:'none',
        
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import "../styles/header.css";
 
 
+
 const Header = () => {
     return (
         <>
             <div className="blockContainer">
                 <div className="header">
-                    <img className="banniere" src="./assets/header-img.jpg" alt="image de la plage" />
+                    <img className="banniere" src="./assets/header.jpg" alt="image de la plage" />
                     <h2 className="title">locationbarcares.fr</h2>
                     <h3 className="describe">Appartement T2 cabine vue mer Wifi</h3>
+                   
+                    <img className="diapo" src="./assets/image1.jpg" alt="image de la plage" />
+                    <img className="diapo" src="./assets/image1.jpg" alt="image de la plage" />
                 </div>
             </div>
         </>
